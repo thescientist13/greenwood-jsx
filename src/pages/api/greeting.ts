8fded707ab4590309fa83b9e94c0a22479d70f53
Owen Buckley <owenbuckley13@gmail.com>
2025-10-25 13:46:12 -0400

    basic demo

---
+++ src/pages/api/greeting.ts
@@ -0,0 +1,13 @@
+import { getMessage } from '../../services/message.js';
+
+export async function handler(request: Request) {
+  const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
+  const name = params.has('name') ? params.get('name') : 'Greenwood';
+  const body = { message: getMessage(name) };
+
+  return new Response(JSON.stringify(body), {
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  });
+}
